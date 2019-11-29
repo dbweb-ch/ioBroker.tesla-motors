af944d861cb855c5294868b0032025fd89baa5f5
dominic.blattmann <dominic.blattmann@prolistic.ch>
2019-11-29 13:17:31 +0100

    Add Translations

--- admin/words.js
+++ admin/words.js
@@ -2,72 +2,184 @@
 'use strict';
 
 systemDictionary = {
-    'Tesla Account User': {
-        'en': 'Tesla Account User (Usually your Mailaddress)',
-        'de': 'Tesla Benutzername (Normalerweise deine Mailadresse)'
+    "Tesla Account User (Usually your Mailaddress)": {
+        "en": "Tesla Account User (Usually your Mailaddress)",
+        "de": "Tesla Benutzername (normalerweise Ihre Mailadresse)",
+        "ru": "Пользователь учетной записи Tesla (обычно ваш почтовый адрес)",
+        "pt": "Usuário da conta Tesla (geralmente o seu endereço de email)",
+        "nl": "Tesla-accountgebruiker (meestal uw e-mailadres)",
+        "fr": "Compte utilisateur Tesla (généralement votre adresse mail)",
+        "it": "Utente dell'account Tesla (di solito il tuo indirizzo di posta)",
+        "es": "Usuario de la cuenta de Tesla (generalmente su dirección de correo electrónico)",
+        "pl": "Użytkownik konta Tesla (zazwyczaj adres pocztowy)",
+        "zh-cn": "Tesla帐户用户（通常是您的邮件地址）"
     },
-    'Tesla Account Password': {
-        'en': 'Tesla Password',
-        'de': 'Tesla Passwort'
+    "Tesla Password": {
+        "en": "Tesla Password",
+        "de": "Tesla Passwort",
+        "ru": "Тесла Пароль",
+        "pt": "Senha Tesla",
+        "nl": "Tesla-wachtwoord",
+        "fr": "Tesla Mot de passe",
+        "it": "Tesla Password",
+        "es": "Contraseña de Tesla",
+        "pl": "Hasło Tesli",
+        "zh-cn": "特斯拉密码"
     },
     "Smart": {
         "en": "Smart",
-        "de": "Smart",
+        "de": "Clever",
+        "ru": "Умная",
+        "pt": "Inteligente",
+        "nl": "Slim",
+        "fr": "Intelligent",
+        "it": "Inteligente",
+        "es": "Inteligente",
+        "pl": "Mądry",
+        "zh-cn": "聪明"
     },
     "Aggressive": {
         "en": "Aggressive",
         "de": "Aggressiv",
+        "ru": "агрессивный",
+        "pt": "Agressivo",
+        "nl": "Agressief",
+        "fr": "Agressif",
+        "it": "Aggressivo",
+        "es": "Agresivo",
+        "pl": "Agresywny",
+        "zh-cn": "好斗的"
     },
     "Temperate": {
         "en": "Temperate",
         "de": "Zurückhaltend",
+        "ru": "умеренный",
+        "pt": "Temperado",
+        "nl": "Gematigd",
+        "fr": "Tempéré",
+        "it": "Temperato",
+        "es": "Templado",
+        "pl": "Umiarkowany",
+        "zh-cn": "温和的"
     },
     "Off": {
         "en": "Off",
-        "de": "Aus",
+        "de": "aus",
+        "ru": "от",
+        "pt": "Fora",
+        "nl": "Uit",
+        "fr": "De",
+        "it": "via",
+        "es": "Apagado",
+        "pl": "Poza",
+        "zh-cn": "关"
     },
     "Refresh Rate": {
         "en": "Refresh Rate",
         "de": "Aktualisierungsrate",
+        "ru": "Частота обновления",
+        "pt": "Taxa de atualização",
+        "nl": "Verniewings snelheid",
+        "fr": "Fréquence de rafraîchissement",
+        "it": "Frequenza di aggiornamento",
+        "es": "Frecuencia de actualización",
+        "pl": "Częstotliwość odświeżania",
+        "zh-cn": "刷新率"
     },
     "Save Credentials": {
         "en": "Save Credentials",
-        "de": "Benutzerdaten speichern",
+        "de": "Anmeldeinformationen speichern",
+        "ru": "Сохранить учетные данные",
+        "pt": "Salvar credenciais",
+        "nl": "Referenties opslaan",
+        "fr": "Enregistrer les informations d'identification",
+        "it": "Salva credenziali",
+        "es": "Guardar credenciales",
+        "pl": "Zapisz poświadczenia",
+        "zh-cn": "保存凭证"
     },
     "Get Token": {
         "en": "Get Token",
         "de": "Token holen",
+        "ru": "Получить токен",
+        "pt": "Obter token",
+        "nl": "Ontvang een token",
+        "fr": "Obtenir un jeton",
+        "it": "Ottieni token",
+        "es": "Obtener token",
+        "pl": "Zdobądź Token",
+        "zh-cn": "获取令牌"
     },
     "Token": {
         "en": "Token",
         "de": "Token",
+        "ru": "знак",
+        "pt": "Símbolo",
+        "nl": "blijk",
+        "fr": "Jeton",
+        "it": "Gettone",
+        "es": "Simbólico",
+        "pl": "Znak",
+        "zh-cn": "代币"
     },
     "Refresh Token": {
         "en": "Refresh Token",
         "de": "Aktualisierungs Token",
+        "ru": "Обновить токен",
+        "pt": "Atualizar token",
+        "nl": "Token vernieuwen",
+        "fr": "Refresh Token",
+        "it": "Aggiorna token",
+        "es": "Actualizar token",
+        "pl": "Odśwież token",
+        "zh-cn": "刷新令牌"
     },
     "Expires on": {
         "en": "Expires on",
         "de": "Läuft ab am",
+        "ru": "Годен до",
+        "pt": "Expira em",
+        "nl": "Verloopt op",
+        "fr": "Expire le",
+        "it": "Scade il",
+        "es": "Expira el",
+        "pl": "Wygasa w dniu",
+        "zh-cn": "到期"
     },
     "Select your Car": {
         "en": "Select your Car",
-        "de": "Wähle dein Auto",
-    },
-    "Receive extended States": {
-        "en": "Receive extended States",
-        "de": "Erweiterte Datenpunkte",
+        "de": "Wählen Sie Ihr Auto",
+        "ru": "Выберите свой автомобиль",
+        "pt": "Selecione seu carro",
+        "nl": "Selecteer uw auto",
+        "fr": "Sélectionnez votre voiture",
+        "it": "Seleziona la tua auto",
+        "es": "Selecciona tu auto",
+        "pl": "Wybierz swój samochód",
+        "zh-cn": "选择你的车"
     },
     "You have to get a Token before storing!": {
         "en": "You have to get a Token before storing!",
-        "de": "Du musst zuerst ein Token holen, bevor du speichern kannst",
+        "de": "Sie müssen ein Token holen, bevor sie speichern können!",
+        "ru": "Вы должны получить токен перед хранением!",
+        "pt": "Você precisa obter um token antes de armazenar!",
+        "nl": "Je moet een teken krijgen voordat je het opbergt!",
+        "fr": "Vous devez obtenir un jeton avant de le stocker!",
+        "it": "Devi ottenere un token prima di archiviarlo!",
+        "es": "¡Tienes que conseguir un token antes de guardarlo!",
+        "pl": "Przed przechowaniem musisz zdobyć token!",
+        "zh-cn": "您必须先获得令牌，然后才能存储！"
     },
     "Please start the Adapter first": {
         "en": "Please start the Adapter first",
-        "de": "Bitte zuerst den Adapter starten",
-    },
-    "teststring": {
-        "en": "EN teststring",
-        "de": "DE teststring",
+        "de": "Bitte starten Sie zuerst den Adapter",
+        "ru": "Пожалуйста, сначала запустите адаптер",
+        "pt": "Por favor, inicie o adaptador primeiro",
+        "nl": "Start eerst de adapter",
+        "fr": "S'il vous plaît commencer par l'adaptateur",
+        "it": "Si prega di avviare prima l'adattatore",
+        "es": "Por favor, inicie el adaptador primero",
+        "pl": "Najpierw uruchom adapter",
+        "zh-cn": "请先启动适配器"
     }
 };
